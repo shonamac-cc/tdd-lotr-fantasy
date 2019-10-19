0252915b6c22f586cf4e41114338692246c260e6
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-19 19:03:25 +0100

    added test that Frodo can damage Azog the Orc

--- src/test/java/HobbitTest.java
+++ src/test/java/HobbitTest.java
@@ -1,16 +1,23 @@
 import org.junit.Before;
 import org.junit.Test;
+import players.enemies.Orc;
 import players.fellowship.Hobbit;
+import weapons.Sword;
+import weapons.Weapon;
 
 import static org.junit.Assert.assertEquals;
 
 public class HobbitTest {
 
     Hobbit hobbit;
+    Orc orc;
+    Sword sword;
 
     @Before
     public void before(){
         hobbit = new Hobbit();
+        orc = new Orc();
+        sword = new Sword();
     }
 
     @Test
@@ -18,4 +25,11 @@ public class HobbitTest {
         hobbit.addWeapons();
         assertEquals(1, hobbit.getWeapons().size());
     }
+
+    @Test
+    public void canDamageEnemy(){
+        orc.damage(sword);
+        assertEquals(85, orc.getHPoints());
+    }
+
 }
